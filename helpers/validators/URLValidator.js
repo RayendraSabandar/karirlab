f9d42bd8a280f97cbef4e36fee8a13aad279f3b5
Rayendra <rayendrasabandar@github.com>
2022-06-19 21:19:09 +0700

    Changed return value to true

--- helpers/validators/URLValidator.js
+++ helpers/validators/URLValidator.js
@@ -1,12 +1,12 @@
 function linkedInValidator(linkedin_url) {
-    var result = false
+    var result = true
     if (!result) {
         throw new Error('Must enter a valid LinkedIn URL')
     }
 }
 
 function portfolioValidator(portfolio_url){
-    var result = false
+    var result = true
     if(!result){
         throw new Error('Must enter a valid Portfolio URL')
     }
