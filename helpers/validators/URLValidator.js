1534a5d15f6b399cbb3853af8326a87640720f68
Rayendra <rayendrasabandar@github.com>
2022-06-20 21:38:18 +0700

    Added error message for URL validator

--- helpers/validators/URLValidator.js
+++ helpers/validators/URLValidator.js
@@ -1,14 +1,14 @@
 function linkedInValidator(linkedin_url) {
     var linkedInRegex = /^((http:|https:)\/\/(www\.|)|linkedin.com\/in|www.|)(linkedin.com\/in\/)[A-Za-z0-9-]+$/
     if(!linkedInRegex.test(linkedin_url)) {
-        throw new Error('Must enter a valid LinkedIn URL');
+        throw new Error(`Must enter a valid LinkedIn URL; Remove the key if you didn't want to put a valid URL`);
     } 
 }
 
 function portfolioValidator(portfolio_url){
     var urlRegex = /^(https:\/\/www\.|http:\/\/www\.|www\.|https:\/\/|http:\/\/)[a-zA-Z0-9_-]+\.[a-zA-Z]{2,5}$/
     if(!urlRegex.test(portfolio_url)) {
-        throw new Error('Must enter a valid Portfolio URL')
+        throw new Error(`Must enter a valid Portfolio URL; Remove the key if you didn't want to put a valid URL`)
     }
 }
 
