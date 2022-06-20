11f3ab7e7ebfc3b0538670924ce027c8f5f7f80d
Rayendra <rayendrasabandar@github.com>
2022-06-20 20:39:36 +0700

    Created validators

--- helpers/validators/URLValidator.js
+++ helpers/validators/URLValidator.js
@@ -1,18 +1,16 @@
 function linkedInValidator(linkedin_url) {
-    var result = true
-    if (!result) {
-        throw new Error('Must enter a valid LinkedIn URL')
-    }
+    var linkedInRegex = /^((http:|https:)\/\/(www\.|)|linkedin.com\/in|www.|)(linkedin.com\/in\/)[A-Za-z0-9-]+$/
+    if(!linkedInRegex.test(linkedin_url)) {
+        throw new Error('Must enter a valid LinkedIn URL');
+    } 
 }
 
 function portfolioValidator(portfolio_url){
-    var result = true
-    if(!result){
+    var urlRegex = /^(https:\/\/www\.|http:\/\/www\.|www\.|https:\/\/|http:\/\/)[a-zA-Z0-9_-]+\.[a-zA-Z]{2,5}$/
+    if(!urlRegex.test(portfolio_url)) {
         throw new Error('Must enter a valid Portfolio URL')
     }
 }
-
-// Coba kelarin
 
 module.exports = {
     linkedInValidator,
